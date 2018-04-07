8fc79ed47c57eeddb9a8b019f20ce0947df3e6f9
Indiver Kumar <indiver.kumar@nagarro.com>
2018-04-08 02:11:21 +0530

    dude

--- update-service.sh
+++ update-service.sh
@@ -7,4 +7,4 @@ OLD_TASK_DEF=$(aws ecs describe-task-definition --task-definition $TASK_FAMILY -
 NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $NEW_DOCKER_IMAGE '.taskDefinition.containerDefinitions[0].image=$NDI')
 FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions}')
 aws ecs register-task-definition --family $TASK_FAMILY --cli-input-json "$(echo $FINAL_TASK)"
-aws ecs update-service --service $SERVICE_NAME --task-definition $TASK_FAMILY --cluster $CL
+aws ecs update-service --service $SERVICE_NAME --task-definition $TASK_FAMILY --cluster $CLUSTER_NAME
